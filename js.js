92bb7f6f0bb7a577a4b92410ab8054b3d6c2b72d
aha00a <aha00a@gmail.com>
2026-04-24 20:20:08 +0900

    use secure password generation, improve input events handling, and fix SQL syntax

--- js.js
+++ js.js
@@ -8,9 +8,19 @@ function getParameterByName(name) {
 $(function(){
     function generatePassword() {
         var chars = "abcdefghijklmnopqrstuvwxyz0123456789";
+        var randomValues = new Uint32Array(32);
         var result = "";
-        for (var i = 0; i < 32; i++) {
-            result += chars.charAt(Math.floor(Math.random() * chars.length));
+
+        if (window.crypto && window.crypto.getRandomValues) {
+            window.crypto.getRandomValues(randomValues);
+        } else {
+            for (var i = 0; i < randomValues.length; i++) {
+                randomValues[i] = Math.floor(Math.random() * 4294967296);
+            }
+        }
+
+        for (var j = 0; j < randomValues.length; j++) {
+            result += chars.charAt(randomValues[j] % chars.length);
         }
         return result;
     }
@@ -35,23 +45,23 @@ $(function(){
         $pw.val(generatePassword()).trigger('input');
     });
 
-    $('input').bind('input cut paste keydown keyup keypress blur', function () {
+    $('input').on('input keydown', function () {
         var s8 = $('.template8').val();
         $.each(["database", "id", "pw"], function (index, name) {
             var value = $('[name=' + name + ']').val();
             s8 = s8.replace(new RegExp("__" + name + "__", "g"), value);
         });
-        $('.result8').html(s8);
+        $('.result8').text(s8);
 
         var s5 = $('.template5').val();
         $.each(["database", "id", "pw"], function (index, name) {
             var value = $('[name=' + name + ']').val();
             s5 = s5.replace(new RegExp("__" + name + "__", "g"), value);
         });
-        $('.result5').html(s5);
+        $('.result5').text(s5);
 
 
         var url = '?' + $('.form').serialize();
         $('.shareUrl').attr('href', url).text(url);
-    }).keydown();
+    }).trigger('input');
 });
